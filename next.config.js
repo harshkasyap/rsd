23ff80c1f0fc2db57fe7b081b01e072ce2f535ae
Harsh Kasyap <harshkasyap@gmail.com>
2019-12-20 12:39:22 +0530

    name change

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = {
     }
   },
   //assetPrefix: '',
-  assetPrefix: !debug ? '/Next-gh-page-example/' : '',
+  assetPrefix: !debug ? '/rsd/' : '',
   webpack: (config, { dev }) => {
     // Perform customizations to webpack config
     // console.log('webpack');
