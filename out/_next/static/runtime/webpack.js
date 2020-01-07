5c0bc96d6ce910ab3cd7d211bc4cc001edd9afbf
Harsh Kasyap <harshkasyap@gmail.com>
2020-01-07 15:23:00 +0530

    work by Jan7

--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "93cbd108cefbff462570";
+/******/ 	var hotCurrentHash = "81da5aa9d0796cc3cb81";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
